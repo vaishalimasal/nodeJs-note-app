4ba107eeb57b141a83b9d51820ce9176f7f4d71d
vaishalimasal <98303984+vaishalimasal@users.noreply.github.com>
2022-09-08 17:14:29 +0530

    ad patchByid data

--- controller/notes.js
+++ controller/notes.js
@@ -10,10 +10,35 @@ export const addNotepadData = (req, res)=>{
     res.send("Successfully added note to the DB");
 }
 
-export const getbyIdNotepadData = (req, res)=> {
+export const getByIdNotepadData = (req, res)=> {
         console.log(req.params.id);
         const result = notepadData.filter(
           (notes) => notes.id === parseInt(req.params.id)
         );
         res.send(result);
       }
+
+export const patchByIdNotepadDatah = (req, res) => {
+            const { body, params } = req;
+            console.log(req.params.id);
+            notepadData.forEach((notes) => {
+              if (notes.id === parseInt(params.id)) {
+                if (body.title) {
+                  notes.title = body.title;
+                }
+              }
+            });
+            res.send("Successfully updated note to the DB");
+          };
+
+export const deleteByIdNotepadDatah = (req, res) => {
+                const idx = notepadData.findIndex(
+                  (notes) => notes.id === parseInt(req.params.id)
+                );
+                if (idx >= 0) {
+                  delete notepadData[idx];
+                }
+                res.send("Successfully deteled note to the DB");
+              };
+            
+          
--- router/notes.js
+++ router/notes.js
@@ -1,44 +1,15 @@
 import express from 'express'
-import { addNotepadData, getAllNotepadData, getbyIdNotepadData} from '../controller/notes.js';
+import { addNotepadData, getAllNotepadData, getByIdNotepadData, patchByIdNotepadDatah , deleteByIdNotepadDatah} from '../controller/notes.js';
 const router = express.Router();
 
 router.get("/", getAllNotepadData);
 
-router.get("/:id", getbyIdNotepadData);
+router.get("/:id", getByIdNotepadData);
 
+router.post("/", addNotepadData);
 
-// router.get("/:id", (req, res) => {
-//     console.log(req.params.id);
-//     const result = notepadData.filter(
-//       (notes) => notes.id === parseInt(req.params.id)
-//     );
-//     res.send(result);
-//   });
-  
-//   router.patch("/:id", (req, res) => {
-//     const { body, params } = req;
-//     console.log(req.params.id);
-//     notepadData.forEach((notes) => {
-//       if (notes.id === parseInt(params.id)) {
-//         if (body.title) {
-//           notes.title = body.title;
-//         }
-//       }
-//     });
-//     res.send("Successfully updated note to the DB");
-//   });
-  
-  router.post("/", addNotepadData);
-  
-//   router.delete("/:id", (req, res) => {
-//     const { params } = req;
-//     const idx = notepadData.findIndex(
-//       (notes) => notes.id === parseInt(req.params.id)
-//     );
-//     if (idx >= 0) {
-//       delete notepadData[idx];
-//     }
-//     res.send("Successfully deteled note to the DB");
-//   });
+router.patch("/:id", patchByIdNotepadDatah)  //to edit a particular data 
+
+// router.delete("/:id", deleteByIdNotepadDatah)
 
 export default router;
